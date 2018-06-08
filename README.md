299a6947e243a80381d60085ded4be98e20c1ad3
Aliciar-Thornthwaite <aliciathornthwaite@gmail.com>
2018-06-08 13:37:14 +0100

    ReadMe and Beautifier added
    jshint used to look for errors
    jsbeautifier used to check formatting and ReadMe file inserted

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
 # feed-reader---P5
-5th Project from Udacity Course
+Alicia Thornthwaite -- 5th Project from Udacity Course
+Version 1
+
+PROJECT SPECIFICATIONS
+
+Write a test that creates a loop through all Feeds and makes sure URL is defined:
+For this test a 'for loop' was created after examining apps.js where the function loadFeed and variables are defined. From this allFeeds[id].url can be used to determine if URL is defined using jasmine toBeDefined and that the .length of the object cannot be 0.
+
+Write a test that creates a loop through all Feeds and makes sure name is defined:
+For this test a 'for loop' was created after examining apps.js where the function loadFeed and variables are defined. From this allFeeds[id].name can be used to determine if Name is defined using jasmine's 'toBeDefined' and that the .length of the object cannot be equal to 0.
+
+ A new test suite called 'The Menu' was created.
+
+Write a test that ensures the menu element is hidden by default:
+This test shows that the menu is disabled and/or shown by adding and removing the .menu-hidden class. To test its presence then the class menu-hidden (.hasClass) should be in the body of index.html.
+Show that .menu-hidden is true using .hasClass (to determine whether element has the class menu-hidden)
+
+Write a test that ensures the menu changes visibility when the menu icon is clicked:
+This test checks that on click of the menu icon the menu is shown and then disabled on a second click. In app.js .menu-icon-link uses toggle class to add and remove the .menu-hidden class.
+
+Two tests will be required in this test suite. One to show that .menu-hidden is false using .hasClass so menu is hidden. and one to show that .menu-hidden is true using .hasClass so menu is visible.
+
+A new test suite called 'Initial Entries' was created.
+
+Write a test that ensures when the `loadFeed` function is called and completes its work, there is at least a single `.entry` element within the `.feed` container:
+loadFeed is asynchronous so we need to use the done() function to make sure this is completed first.
+Then test that the number of entries in the .entry class is more than 0 entries using jasmine .toBeGreaterThan.
+
+A new test suite called 'New Feed Selection' was created.
+
+Write a test that ensures when a new feed is loaded by the `loadFeed` function that the content actually changes:
+loadFeed is asynchronous so we need to use the done() function to make sure loadFeed(0) and loadFeed(1) are completed first .
+Then test that first feeds entry does not equal the new feeds entry using jasmine .not.toEqual.
+
+All Tests then Pass
+
+Styling
+Application uses CSS to style components for the game.
+
+Usability
+All application components are usable across modern desktop, tablet, and phone browsers.
+
+Documentation
+CRITERIA
+MEETS SPECIFICATIONS
+README
+A README file is included detailing the game and all dependencies.
+
+Comments
+Comments are present and effectively explain longer code procedure when necessary.
+
+Code Quality
+Code is formatted with consistent, logical, and easy-to-read formatting as described in the Udacity JavaScript Style Guide.
+
+
+Prerequisites
+You will need a browser window to see the feed at https://alicethorn.github.io/feed-reader---P5/
+and files at
+https://github.com/AliceThorn/feed-reader---P5
+
+
+Built With
+Atom
+
+Versioning
+Git Repo
+
+Authors
+Alicia Thornthwaite
+
+
+Acknowledgments and Credits
+Team Udacity and my Mentor for all their help
+http://jsbeautifier.org/
+http://jshint.com/
+https://jasmine.github.io/2.4/introduction#section-Included_Matchers
+https://devhints.io/jasmine
+Reviewer Notes on
+https://discussions.udacity.com/t/new-feed-selection-question/797040
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -31,33 +30,33 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-           it('urls are defined', function() {
+        it('urls are defined', function() {
 
-             for (i = 0; i < allFeeds.length; i++) {
-               expect(allFeeds[i].url).toBeDefined();
-               expect(allFeeds[i].url.length).not.toBe(0);
-             }
-            })
+            for (i = 0; i < allFeeds.length; i++) {
+                expect(allFeeds[i].url).toBeDefined();
+                expect(allFeeds[i].url.length).not.toBe(0);
+            }
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
 
-           it('names are defined', function() {
+        it('names are defined', function() {
 
-             for (j = 0; j < allFeeds.length; j++) {
-               expect(allFeeds[j].name).toBeDefined();
-               expect(allFeeds[j].name.length).not.toBe(0);
-             }
-           })
+            for (j = 0; j < allFeeds.length; j++) {
+                expect(allFeeds[j].name).toBeDefined();
+                expect(allFeeds[j].name.length).not.toBe(0);
+            }
+        });
 
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
 
-          describe('The Menu', function() {
+    describe('The Menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -65,36 +64,36 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-          it('is hidden by default', function(){
+        it('is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true)
-          })
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-
-          //it('displays when icon clicked', function(){
-          //  $('menu-icon-link').click();
-            //expect($('body')).hasClass('.menu-hidden').toBe(false)
-          //  expect($('body')).toHaveClass('menu-hidden')
-        //  })
-
-          it('displays when icon is clicked', function(){
-            $('.menu-icon-link').click();
-            expect($('body').hasClass('menu-hidden')).toBe(false)
-          })
-
-          it('hidden when icon is clicked again', function(){
-            $('.menu-icon-link').click();
-            expect($('body').hasClass('menu-hidden')).toBe(true)
-          })
-
         });
 
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
+
+        //it('displays when icon clicked', function(){
+        //  $('menu-icon-link').click();
+        //expect($('body')).hasClass('.menu-hidden').toBe(false)
+        //  expect($('body')).toHaveClass('menu-hidden')
+        //  })
+
+        it('displays when icon is clicked', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false)
+        });
+
+        it('hidden when icon is clicked again', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true)
+        });
+
+    });
+
     /* TODO: Write a new test suite named "Initial Entries" */
-          describe('Initial Entries', function() {
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -102,44 +101,44 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-         beforeEach(function(done) {
-         loadFeed(0, function(){
-         done();
-       })
-      });
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
 
-         it('contains at least one entry in feed', function(){
-           expect($('.entry').length).toBeGreaterThan(0)
-         })
-       });
+        it('contains at least one entry in feed', function() {
+            expect($('.entry').length).toBeGreaterThan(0)
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-      describe('New Feed Selection', function() {
+    describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
 
-// As per reviwer notes on https://discussions.udacity.com/t/new-feed-selection-question/797040
-        beforeEach(function(done){
-      //complete loading of first feed
-          loadFeed(0, function(){
-            done();
-      //complete loading of second /new feed
-          loadFeed(1, function(){
-            done();
+        // As per reviwer notes on https://discussions.udacity.com/t/new-feed-selection-question/797040
+        beforeEach(function(done) {
+            //complete loading of first feed
+            loadFeed(0, function() {
+                done();
+                //complete loading of second /new feed
+                loadFeed(1, function() {
+                    done();
 
-          done();
+                    done();
+                });
+            });
         });
-      });
-    });
 
-//first feeds entry does not equal the new feeds entry
-         it('ensures when a new feed is loaded, the content changes', function(){
+        //first feeds entry does not equal the new feeds entry
+        it('ensures when a new feed is loaded, the content changes', function() {
             expect(allFeeds[0]).not.toEqual(allFeeds[1])
-         })
         });
+    });
 
 
 
